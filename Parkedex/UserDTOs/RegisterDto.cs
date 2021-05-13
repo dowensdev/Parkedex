c091a6027cc8a64aae4760791e0002eef780d565
Derek Owens <dowensdev@gmail.com>
2021-05-13 09:53:15 -0400

    Fixed error where password length was constrained to 4 to 8 characters. Modified to 8 to 32.

--- Parkedex/UserDTOs/RegisterDto.cs
+++ Parkedex/UserDTOs/RegisterDto.cs
@@ -12,8 +12,8 @@ namespace API.UserDTOs
         [EmailAddress]
         public string Email { get; set; }
         [Required]
-        [RegularExpression("(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[#?!@$%^&*-]).{4,8}$", 
-            ErrorMessage="Password must include uppercase, lowercase, number, and a special character. Please try again.")]
+        [RegularExpression("(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[#?!@$%^&*-]).{8,32}$", 
+            ErrorMessage="Password must include uppercase, lowercase, number, and a special character, minimum 8 characters. Please try again.")]
         public string Password { get; set; }
     }
 }
